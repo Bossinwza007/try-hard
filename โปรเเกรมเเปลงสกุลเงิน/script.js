f57d2ded7f4a77d8e20c681a02210f7e2dead016
Bossinwza <boss190945@gmail.com>
2023-06-22 10:46:36 +0700

    update

--- โปรเเกรมเเปลงสกุลเงิน/script.js
+++ โปรเเกรมเเปลงสกุลเงิน/script.js
@@ -17,9 +17,9 @@ amount_two.addEventListener('input',calculateMoney);
 function calculateMoney(){
     const one = currency_one.value;
     const two = currency_two.value;
-    fetch(`https://api.exchangerate-api.com/v4/latest/${one}`)
-    .then(res=>res.json()).then(data=>{
-        const rate=data.rates[two];
+    fetch(`https://api.exchangerate-api.com/v4/latest/${one}`) //ดึงข้อมูลได้ค่าเป็น promise
+    .then(res=>res.json()).then(data=>{                        //ต้องการให้เกิดเหตุการณ์อะไรขึ้น //ได้ค่า promise เป็น json เเล้วก็เอาค่ามาทำเป็น data ต่อ
+        const rate=data.rates[two];                            //rates เป็นค่าสกุลเงินใน data
         rateText.innerText=`1 ${one} = ${rate} ${two}`;
         amount_two.value=(amount_one.value*rate).toFixed(2);
     })
